fa15be90b855cc6ee8598f2a659b59aee4ce2f00
albus2005 <aristotelu2005@gmail.com>
2026-06-10 16:55:50 +0200

    essai 2

---
+++ afrodata_1_j29v012.py
@@ -0,0 +1,31 @@
+# IMPORTATIONS
+import os 
+import pandas as pd 
+
+
+'''
+AFRODATA 1 v011
+Traitement et analyse des données excel sur l'epidemie d'Ebola 2026
+
+1. Charger le fichier Excel
+creer le chemin du ficher excel 
+retourner un dataframe et une EDA 
+
+2. Nettoyer -- valeurs manquantes, colonnes inutiles
+3. Filtrer -- garder seulement les zones actives (cas > 0)
+4. Agréger -- totaux par province
+5. Afficher -- indicateurs clés en console
+
+'''
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+path_file = os.path.join(BASE, "data", "drc_ebola_cases_20mai.xlsx")
+
+# FONCTION CHARGER 
+def excel_loader(path) :
+    df = pd.read_excel(path)
+    
+    return df.head(10)
+    
+excel_loader(path_file)
+
